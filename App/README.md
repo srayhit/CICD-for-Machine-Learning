391bcd82800e69b6cab52535ccdb5f9edc3cbce7
srayhit <sambarta1202@outlook.com>
2025-10-18 17:07:57 -0700

    Added the file/folders structure

---
+++ App/README.md
@@ -0,0 +1,26 @@
+# CICD-for-Machine-Learning
+
+A repository to automate training, evaluation and deploying models using Github Actions.
+
+
+
+title: Drug Classification
+
+emoji: 📈
+
+colorFrom: indigo
+
+colorTo: yellow
+
+sdk: gradio
+
+sdk\_version: 5.49.1
+
+app\_file: app.py
+
+pinned: false
+
+license: apache-2.0
+
+short\_description: A classifier for predicting prescription drugs
+
---
+++ train.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score
+
+import matplotlib.pyplot as plt
+
+import skops.io as sio
+
+#Loading Data
+
+drug_df = pd.read_csv("Data/drug200.csv")   # Load the dataset
+drug_df = drug_df.sample(frac=1)# Shuffle the data
+drug_df.head(3)# Display first 3 rows
+
+#Feature Engineering
+
+X = drug_df.drop("Drug", axis=1).values
+y = drug_df.Drug.values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=125)
+
+cat_col = [1,2,3] # Categorical columns
+num_col = [0, 4] #numerical columns
+
+transform = ColumnTransformer(
+    [
+        ("encoder", OrdinalEncoder(), cat_col),
+        ("num_imputer", SimpleImputer(strategy="median"), num_col),
+        ("num_scaler", StandardScaler(), num_col)
+    ]
+)
+
+pipe = Pipeline(
+    steps = [
+        ("preprocessing", transform),
+        ("model", RandomForestClassifier(n_estimators=100, random_state=125))
+    ]
+)
+pipe.fit(X_train, y_train)
+
+#Running Pipeline for prediciton and evaluation
+
+predictions = pipe.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+f1 = f1_score(y_test, predictions, average="macro")
+
+print(f"Accuracy: {accuracy:.2f}%, F1: {f1:.2f}")
+
